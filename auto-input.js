92b44216e41019b08e5ec6ad4e8e6dcad3af1fcc
hiroki.dekura <hiroki.dekura@insight-lab.co.jp>
2019-08-29 17:14:40 +0900

    purge unused logic

--- auto-input.js
+++ auto-input.js
@@ -19,16 +19,12 @@ const path = require('path');
     await page.goto(url_logon);
     await page.type('input[name="user_name"]', user_name);
     await page.type('input[name="password"]', user_secret);
-    // await page.$eval('input[name="user_name"]', el => el.value = user_name);
-    // await page.$eval('input[name="password"]', el => el.value = user_secret);
-    //await page.screenshot({path: 'screenshot_login.png'});
     await page.click('#submit1')
   }
   
   // menu screen
   {
     await page.waitFor('img[class="logo-image"]', {timeout: 120000});
-    //await page.screenshot({path: 'screenshot_main.png'});
   }
 
   // nippou screen
@@ -40,7 +36,6 @@ const path = require('path');
     await frame.select('select[name="time_in_hour"]', hour_start);
     await frame.select('select[name="time_out_hour"]', hour_end);
     await frame.select('select[name="time_break_input_hour"]', '1');
-    //await page.screenshot({path: 'screenshot_nippou.png'});
     await frame.click('#button5');
   }
 
@@ -48,8 +43,5 @@ const path = require('path');
   {
     const frame = await page.frames().find(f => f.name() === 'classic_window');
     await frame.waitFor('select[name="time_in_hour"]', {timeout: 120000});
-    //await page.screenshot({path: 'screenshot_nippou_saved.png'});
   }
-
-  // await browser.close();
 })();
